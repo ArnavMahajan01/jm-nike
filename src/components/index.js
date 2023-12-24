b5bdd039fbd03d7b9deaf05fae7e132ff0f6e540
ArnavMahajan01 <arnav.mahajan.2001@gmail.com>
2023-12-24 22:39:29 +0530

    Added Footer

--- src/components/index.js
+++ src/components/index.js
@@ -2,5 +2,6 @@ import Button from "./Button";
 import Nav from "./Nav";
 import ShoeCard from "./ShoeCard";
 import PopularProductCard from "./PopularProductCard";
+import ReviewCard from "./ReviewCard";
 
-export { Button, Nav, ShoeCard, PopularProductCard };
+export { Button, Nav, ShoeCard, PopularProductCard, ReviewCard };
